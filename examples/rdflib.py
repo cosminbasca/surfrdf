8433de369add50c6243d08e102178209ef8781e1
Cosmin Basca <cosmin.basca@gmail.com>
2009-09-20 16:57:09 +0000

    rdflib example

---
+++ examples/rdflib.py
@@ -0,0 +1,29 @@
+from surf import *
+
+store = Store(  reader='rdflib',
+                writer='rdflib',
+                rdflib_store = 'IOMemory')
+
+session = Session(store)
+
+#print 'load : http://bigasterisk.com/foaf.rdf'
+#store.load_triples(source='http://bigasterisk.com/foaf.rdf')
+print 'load : http://www.w3.org/People/Berners-Lee/card.rdf'
+store.load_triples(source='http://www.w3.org/People/Berners-Lee/card.rdf')
+print 'load : http://danbri.livejournal.com/data/foaf'
+store.load_triples(source="http://danbri.livejournal.com/data/foaf")
+
+#print 'loading http://irc.sioc-project.org/users/CaptSolo#user '
+#store.load_triples(source='http://irc.sioc-project.org/users/CaptSolo#user')
+
+Person = session.get_class(ns.FOAF['Person'])
+
+all_persons = Person.all()
+
+print 'Found %d persons'%(len(all_persons))
+for person in all_persons:
+    print person.foaf_name, ' knows ',person.foaf_knows
+    
+#for s,p,o in store.reader.graph:
+#    print s,p,o
+
