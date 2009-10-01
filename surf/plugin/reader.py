c88b71fc7bf9c37b6d514349002939c93d477340
Peteris Caune <cuu508@gmail.com>
2009-10-01 11:11:41 +0000

    Docstring cleanup.

--- surf/plugin/reader.py
+++ surf/plugin/reader.py
@@ -38,125 +38,197 @@ __author__ = 'Cosmin Basca'
 from surf.plugin import Plugin
 
 class RDFReader(Plugin):
-    '''
-    super class for all surf Reader Plugins
-    '''
+    """ Super class for all surf Reader plugins. """
+
     #protected interface
     def _get(self, subject, attribute, direct, context):
-        '''to be implemented by classes that inherit `RDFReader`, is called
-        directly by `get`'''
+        """ To be implemented by classes that inherit `RDFReader`.
+        
+        This method is called directly by :meth:`get`. 
+        
+        """
         
         return None
     
     def _load(self, subject, context):
-        '''to be implemented by classes that inherit `RDFReader`, is called
-        directly by `load`'''
+        """ To be implemented by classes that inherit `RDFReader`.        
+        
+        This method is called directly by :meth:`load`.
+        
+        """
         
         return {}
     
     def _is_present(self, subject, context):
-        '''to be implemented by classes that inherit `RDFReader`, is called
-        directly by `is_present`'''
+        """ To be implemented by classes that inherit `RDFReader`.
+        
+        This method is called directly by :meth:`is_present`.
+        
+        """
         
         return False
     
     def _all(self, concept, limit = None, offset = None, 
              full = False, context = None):
-        '''to be implemented by classes that inherit `RDFReader`, is called
-        directly by `all`'''
+        """ To be implemented by classes that inherit `RDFReader`.
+        
+        This method is called directly by :meth:`all`.
+        
+        """
         
         return []
         
     def _concept(self,subject):
-        '''to be implemented by classes that inherit `RDFReader`, is called
-        directly by `concept`'''
+        """ To be implemented by classes that inherit `RDFReader`.
+        
+        This method is called directly by :meth:`concept`. 
+        
+        """
         
         return None
     
     def _instances_by_attribute(self, concept, attributes, direct, context):
-        '''to be implemented by classes that inherit `RDFReader`, is called
-        directly by `instances_by_attribute`'''
+        """ To be implemented by classes that inherit `RDFReader`.
+        
+        This method is called directly by :meth:`instances_by_attribute`.
+        
+        """
         
         return []
         
     def _instances(self, concept, direct, filter, predicates, context):
-        '''to be implemented by classes that inherit `RDFReader`, is called
-        directly by `instances`'''
+        """ To be implemented by classes that inherit `RDFReader`.
+        
+        This method is called directly by :meth:`instances`.
+        
+        """
+        
         return []
         
     def _instances_by_value(self,concept,direct,attributes):
-        '''to be implemented by classes that inherit `RDFReader`, is called
-        directly by `instances_by_value`'''
+        """ To be implemented by classes that inherit `RDFReader`.
+        
+        This method is called directly by `instances_by_value`. 
+        
+        """
+        
         return []
     
     
     #public interface
     def get(self, resource, attribute, direct):
-        '''Return the `value(s)` of the corresponding `attribute`,
-        if `direct` is False, than the `subject` of the `resource` is considered
-        the `object` of the query'''
+        """ Return the `value(s)` of the corresponding `attribute`.
+        
+        If ``direct`` is `False` then the subject of the ``resource`` is 
+        considered the object of the query. 
+        
+        """
         
         subj = resource.subject if hasattr(resource, 'subject') else resource
         return self._get(subj, attribute, direct, resource.context)
         
     def load(self, resource, direct):
-        '''Fully load the `resource` from the `store`, returns all statements about
-        the `resource`
-        if `direct` is False, than the `subject` of the `resource` is considered
-        the `object` of the query'''
+        """ Fully load the ``resource`` from the `store`.
+        
+        This method returns all statements about the `resource`.
+        
+        If ``direct`` is `False`, then the subject of the ``resource`` 
+        is considered the object of the query
+        
+        """
         
         subj = resource.subject if hasattr(resource, 'subject') else resource
         return self._load(subj, direct, resource.context)
         
     def is_present(self, resource):
-        '''True if the `resource` is present in the `store`, False otherwise'''
+        """ Return `True` if the ``resource`` is present in the `store`. """
         
         subj = resource.subject if hasattr(resource, 'subject') else resource
         return self._is_present(subj, resource.context)
         
     def all(self, concept, limit = None, offset = None, 
             full = False, context = None):
-        '''returns all `uri's` that are `instances` of `concept` within [`limit`,`limit`+`offset`]'''
+        """ Return information about instances of `concept`. 
+        
+        This method would normally return list of URIs. For example::
+        
+            >>> Person = surf.ns.FOAF["Person"]
+            >>> reader = session.default_store.reader
+            >>> reader.all(Person)
+            [rdflib.URIRef('http://p1'), rdflib.URIRef('http://p2')]
+        
+        If parameter ``full`` is set to `True` and reader plugin is instructed
+        to use sub queries (:attr:`surf.store.Store.use_subqueries` is 
+        set to `True`), then this method will return 
+        {subject : attrs_values, ... } dictionary::
+        
+            >>> Person = surf.ns.FOAF["Person"]
+            >>> reader = session.default_store.reader
+            >>> reader.all(Person, full = True)
+            {rdflib.URIRef('http://p1'): { pred  : { value  : class, value2 : class2 },
+                                           pred2 : { value3 : class3 }},
+             ...
+            }   
+                                        
+        """
+        
         con = concept.uri if hasattr(concept, 'uri') else concept
         return self._all(con, limit = limit, offset = offset, 
                          full = full, context = context)
         
-    def concept(self,resource):
-        '''returns the `concept` URI of the following `resource`,
-        `resource` can be a `string` or a `URIRef`'''
+    def concept(self, resource):
+        """ Return the `concept` URI of the following `resource`.
+        
+        `resource` can be a `string` or a `URIRef`.__builtins__
+        
+        """
+        
         subj = resource.subject if hasattr(resource, 'subject') else resource
         return self._concept(subj)
         
     def instances_by_attribute(self, resource, attributes, direct, context):
-        '''returns all `uri's` that are `instances` of `concept` that have the specified `attributes`
-        if `direct` is False, than the `subject` of the `resource` is considered
-        the `object` of the query'''
+        """
+        Return all `URIs` that are instances of ``resource`` and 
+        have the specified `attributes`.
+        
+        If ``direct`` is `False`, than the subject of the ``resource`` 
+        is considered the object of the query. 
+        
+        """
         
         concept = resource.uri if hasattr(resource, 'uri') else resource
         return self._instances_by_attribute(concept, attributes, direct, 
                                             context)
         
     def instances(self, resource, direct, filter, predicates, context):
-        '''returns all `uri's` that are `instances` of `concept` that have the
-        specified `predicates` - a `dict` where the `key` is the shorthand notation,
-        and the `value` is the predicate value
-        if `direct` is False, than the `subject` of the `resource` is considered
-        the `object` of the query
+        """ 
+        Return all `URIs` that are instances of ``resource`` and have the
+        specified `predicates` - a `dict` where the key is the shorthand 
+        notation, and the value is the predicate value.
+        
+        If ``direct`` is `False`, than the subject of the ``resource`` is 
+        considered the object of the query.
         
         .. code-block:: python
             
-            store.instances(resource,True,None,{'foaf_name':'John Doe'})
+            store.instances(resource, True, None, {'foaf_name' : 'John Doe'})
             
-        '''
+        """
+        
         concept = resource.uri if hasattr(resource, 'uri') else resource
         #TODO: make sure uri's are passed further
         return self._instances(concept, direct, filter, predicates, context)
         
-    def instances_by_value(self,resource,direct,attributes):
-        '''returns all `uri's` that are `instances` of `concept` as values and
-        that have the specified `attributes`
-        if `direct` is False, than the `subject` of the `resource` is considered
-        the `object` of the query'''
+    def instances_by_value(self, resource, direct, attributes):
+        """
+        Return all `URIs` that are instances of ``resource`` as values and
+        that have the specified ``attributes``.
+        
+        If ``direct`` is `False`, than the subject of the ``resource`` is 
+        considered the object of the query.
+        
+        """
+        
         concept = resource.uri if hasattr(resource, 'uri') else resource
         return self._instances_by_value(concept,direct,attributes)
         
