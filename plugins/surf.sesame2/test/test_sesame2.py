77276fe3936c87ddb4f98717345b1f2da4ee3e41
Peteris Caune <cuu508@gmail.com>
2010-01-06 17:50:14 +0000

    Basic test for sesame2 plugin.

---
+++ plugins/surf.sesame2/test/test_sesame2.py
@@ -0,0 +1,39 @@
+""" Module for sparql_protocol plugin tests. """
+
+from unittest import TestCase
+
+import surf
+from surf.query import select
+from surf.rdf import Literal, URIRef
+
+class TestSesame2(TestCase):
+    """ Tests for sesame2 plugin. """
+    
+        
+    def _get_store_session(self):
+        """ Return initialized SuRF store and session objects. """
+        
+        # FIXME: take endpoint from configuration file,
+        # maybe we can mock SPARQL endpoint.
+        store = surf.Store(reader = "sesame2",
+                           writer = "sesame2",
+                           port = "8080",
+                           root_path = "/openrdf-sesame", 
+                           repository = "test")
+
+        session = surf.Session(store)
+        
+        Person = session.get_class(surf.ns.FOAF + "Person")
+        for name in ["John", "Mary", "Jane"]:
+            # Some test data.
+            person = session.get_resource("http://%s" % name, Person)
+            person.foaf_name = name
+            person.save()
+
+        return store, session
+    
+        
+    def test_get_session(self):
+        """ Test that getting store and session works.  """
+        
+        self._get_store_session()
