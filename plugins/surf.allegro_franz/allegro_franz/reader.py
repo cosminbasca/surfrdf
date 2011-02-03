8d924bf92a7ca0edf1f6d8a5977ed7c024d9c8ac
Christoph Burgmer <christoph.burgmer@gmail.com>
2011-02-03 14:55:17 +0000

    Remove prints and unneeded try/except block for import around allegro_franz plugin.
    Add "unsatisfiable" dependeny on franz. Needs to be manually installed.

--- plugins/surf.allegro_franz/allegro_franz/reader.py
+++ plugins/surf.allegro_franz/allegro_franz/reader.py
@@ -39,113 +39,106 @@ from surf.plugin.query_reader import RDFQueryReader
 from allegro_franz.util import toRdfLib
 from surf.rdf import URIRef
 
-try:
-    from franz.openrdf.sail.allegrographserver import AllegroGraphServer
-    from franz.openrdf.repository.repository import Repository
-    from franz.openrdf.query.query import QueryLanguage
-    from franz.openrdf.model import value as sv
-    from franz.openrdf.model import literal as sl
+from franz.openrdf.sail.allegrographserver import AllegroGraphServer
+from franz.openrdf.repository.repository import Repository
+from franz.openrdf.query.query import QueryLanguage
+from franz.openrdf.model import value as sv
+from franz.openrdf.model import literal as sl
 
-    print 'surf.plugin allegro_franz reader : franz libraries installed'
-    class ReaderPlugin(RDFQueryReader):
-        def __init__(self, *args, **kwargs):
-            RDFQueryReader.__init__(self, *args, **kwargs)
+class ReaderPlugin(RDFQueryReader):
+    def __init__(self, *args, **kwargs):
+        RDFQueryReader.__init__(self, *args, **kwargs)
 
-            self.__server = kwargs['server'] if 'server' in kwargs else 'localhost'
-            self.__port = kwargs['port'] if 'port' in kwargs else 6789
-            self.__catalog = kwargs['catalog'] if 'catalog' in kwargs else None
-            self.__repository = kwargs['repository'] if 'repository' in kwargs else None
+        self.__server = kwargs['server'] if 'server' in kwargs else 'localhost'
+        self.__port = kwargs['port'] if 'port' in kwargs else 6789
+        self.__catalog = kwargs['catalog'] if 'catalog' in kwargs else None
+        self.__repository = kwargs['repository'] if 'repository' in kwargs else None
 
-            if not self.__catalog or not self.__repository:
-                raise Exception('Must specify the <catalog> and the <repository> arguments')
+        if not self.__catalog or not self.__repository:
+            raise Exception('Must specify the <catalog> and the <repository> arguments')
 
-            self.__allegro_server = AllegroGraphServer(self.__server, port = self.__port)
-            self.__allegro_catalog = self.__allegro_server.openCatalog(self.__catalog)
-            self.__allegro_repository = self.__allegro_catalog.getRepository(self.__repository, Repository.ACCESS)
-            self.__allegro_repository.initialize()
+        self.__allegro_server = AllegroGraphServer(self.__server, port = self.__port)
+        self.__allegro_catalog = self.__allegro_server.openCatalog(self.__catalog)
+        self.__allegro_repository = self.__allegro_catalog.getRepository(self.__repository, Repository.ACCESS)
+        self.__allegro_repository.initialize()
 
-            self.__con = self.allegro_repository.getConnection()
+        self.__con = self.allegro_repository.getConnection()
 
-        results_format = property(lambda self: 'json')
-        server = property(lambda self: self.__server)
-        port = property(lambda self: self.__port)
-        catalog = property(lambda self: self.__catalog)
-        repository = property(lambda self: self.__repository)
+    results_format = property(lambda self: 'json')
+    server = property(lambda self: self.__server)
+    port = property(lambda self: self.__port)
+    catalog = property(lambda self: self.__catalog)
+    repository = property(lambda self: self.__repository)
 
-        allegro_server = property(lambda self: self.__allegro_server)
-        allegro_catalog = property(lambda self: self.__allegro_catalog)
-        allegro_repository = property(lambda self: self.__allegro_repository)
+    allegro_server = property(lambda self: self.__allegro_server)
+    allegro_catalog = property(lambda self: self.__allegro_catalog)
+    allegro_repository = property(lambda self: self.__allegro_repository)
 
-        def _to_table(self, result):
-            table = []
-            bindings = result.getBindingNames()
-            for bindingSet in result:
-                row = {}
-                for key in bindings:
-                    try:    v = toRdfLib(bindingSet[key])
-                    except: v = None
-                    row[key] = v
-                table.append(row)
-            return table
+    def _to_table(self, result):
+        table = []
+        bindings = result.getBindingNames()
+        for bindingSet in result:
+            row = {}
+            for key in bindings:
+                try:    v = toRdfLib(bindingSet[key])
+                except: v = None
+                row[key] = v
+            table.append(row)
+        return table
 
-        def _ask(self, result):
-            '''
-            returns the boolean value of a ASK query
-            '''
-            return result
+    def _ask(self, result):
+        '''
+        returns the boolean value of a ASK query
+        '''
+        return result
 
-        # execute
-        def _execute(self, query):
-            if query.query_type == 'select':
-                return self.__execute_sparql(unicode(query))
-            elif query.query_type == 'ask':
-                return self.__execute_ask(unicode(query))
+    # execute
+    def _execute(self, query):
+        if query.query_type == 'select':
+            return self.__execute_sparql(unicode(query))
+        elif query.query_type == 'ask':
+            return self.__execute_ask(unicode(query))
 
-        def __execute_ask(self, q_string):
-            boolQuery = self.__con.prepareBooleanQuery(QueryLanguage.SPARQL, q_string)
-            return boolQuery.evaluate()
+    def __execute_ask(self, q_string):
+        boolQuery = self.__con.prepareBooleanQuery(QueryLanguage.SPARQL, q_string)
+        return boolQuery.evaluate()
 
-        def __execute_sparql(self, q_string):
-            self.log.debug(q_string)
-            tupleQuery = self.__con.prepareTupleQuery(QueryLanguage.SPARQL, q_string)
-            tupleQuery.setIncludeInferred(self.inference)
-            return tupleQuery.evaluate()
+    def __execute_sparql(self, q_string):
+        self.log.debug(q_string)
+        tupleQuery = self.__con.prepareTupleQuery(QueryLanguage.SPARQL, q_string)
+        tupleQuery.setIncludeInferred(self.inference)
+        return tupleQuery.evaluate()
 
-        def execute_sparql(self, q_string, format = 'JSON'):
-            results = self.__execute_sparql(q_string)
-            return self._results_to_json(results) if format == 'JSON' else results
+    def execute_sparql(self, q_string, format = 'JSON'):
+        results = self.__execute_sparql(q_string)
+        return self._results_to_json(results) if format == 'JSON' else results
 
-        def close(self):
-            self.__con.close()
+    def close(self):
+        self.__con.close()
 
-        def _results_to_json(self, results):
-            bindings = results.getBindingNames()
-            r_dict = {}
-            r_dict['head'] = {'vars': bindings}
-            r_dict['results'] = {'bindings':[]}
-            for bindingSet in results:
-                json_binding = {}
-                for b in bindings:
-                    value = bindingSet.getValue(b)
-                    if type(value) is sv.URI:
-                        json_binding[b] = {'type':'uri', 'value': value.getURI()}
-                    elif type(value) is sv.BNode:
-                        json_binding[b] = {'type':'bnode', 'value': value.getID()}
-                    elif type(value) is sl.Literal:
-                        dtype = value.getDatatype() if value.getDatatype() else None
-                        lang = value.getLanguage() if value.getLanguage() else None
-                        lit_type = 'typed-literal' if dtype else 'literal'
-                        json_binding[b] = {'type':lit_type, 'value': value.getLabel()}
-                        if dtype:
-                            if type(dtype) in [str, unicode] and dtype.startswith('<') and dtype.endswith('>'):
-                                dtype = dtype.strip('<>')
-                            json_binding[b]['datatype'] = URIRef(dtype)
-                        if lang:
-                            json_binding[b]['xml:lang'] = lang
-                r_dict['results']['bindings'].append(json_binding)
-            return r_dict
-
-except ImportError, e:
-    print 'surf.plugin allegro_franz reader : franz libraries not installed', e
-    class ReaderPlugin(RDFQueryReader):
-        pass
+    def _results_to_json(self, results):
+        bindings = results.getBindingNames()
+        r_dict = {}
+        r_dict['head'] = {'vars': bindings}
+        r_dict['results'] = {'bindings':[]}
+        for bindingSet in results:
+            json_binding = {}
+            for b in bindings:
+                value = bindingSet.getValue(b)
+                if type(value) is sv.URI:
+                    json_binding[b] = {'type':'uri', 'value': value.getURI()}
+                elif type(value) is sv.BNode:
+                    json_binding[b] = {'type':'bnode', 'value': value.getID()}
+                elif type(value) is sl.Literal:
+                    dtype = value.getDatatype() if value.getDatatype() else None
+                    lang = value.getLanguage() if value.getLanguage() else None
+                    lit_type = 'typed-literal' if dtype else 'literal'
+                    json_binding[b] = {'type':lit_type, 'value': value.getLabel()}
+                    if dtype:
+                        if type(dtype) in [str, unicode] and dtype.startswith('<') and dtype.endswith('>'):
+                            dtype = dtype.strip('<>')
+                        json_binding[b]['datatype'] = URIRef(dtype)
+                    if lang:
+                        json_binding[b]['xml:lang'] = lang
+            r_dict['results']['bindings'].append(json_binding)
+        return r_dict
--- plugins/surf.allegro_franz/allegro_franz/util.py
+++ plugins/surf.allegro_franz/allegro_franz/util.py
@@ -35,73 +35,58 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Cosmin Basca'
 
-try:
-    from franz.openrdf.model.value import URI as fURIRef
-    from franz.openrdf.model.value import BNode as fBNode
-    from franz.openrdf.model.literal import Literal as fLiteral
-    
-    from surf.rdf import BNode, Literal, URIRef
-    
-    '''
-    helper functions that convert between rdflib concepts and sesame2 api concepts
-    '''
-    
-    # TERMS
-    def toRdfLib(term):
-        if type(term) is fURIRef:
-            return URIRef(term.getURI())
-        elif type(term) is fLiteral:
-            try:
-                if term.getDatatype() is None:
-                    return Literal(term.getLabel(), lang=term.getLanguage())
+from franz.openrdf.model.value import URI as fURIRef
+from franz.openrdf.model.value import BNode as fBNode
+from franz.openrdf.model.literal import Literal as fLiteral
+
+from surf.rdf import BNode, Literal, URIRef
+
+'''
+helper functions that convert between rdflib concepts and sesame2 api concepts
+'''
+
+# TERMS
+def toRdfLib(term):
+    if type(term) is fURIRef:
+        return URIRef(term.getURI())
+    elif type(term) is fLiteral:
+        try:
+            if term.getDatatype() is None:
+                return Literal(term.getLabel(), lang=term.getLanguage())
+            else:
+                dtype = term.getDatatype().getURI()
+                if dtype.startswith('<') and dtype.endswith('>'):
+                    dtype = dtype.strip('<>')
+                    dtype = URIRef(dtype)
                 else:
-                    dtype = term.getDatatype().getURI()
-                    if dtype.startswith('<') and dtype.endswith('>'):
-                        dtype = dtype.strip('<>')
-                        dtype = URIRef(dtype)
-                    else:
-                        dtype = URIRef(dtype)
-                    
-                    return Literal(term.getLabel(), lang=term.getLanguage(),
-                                   datatype=dtype)
-                    
-            except Exception, e:
-                print e
-        elif type(term) is fBNode:
-            return BNode(term.getID())
-        elif type(term) in [list,tuple]:
-            return map(toRdfLib, term)
-        return term
-    
-    def toSesame(term,factory):
-        if type(term) is URIRef:
-            return factory.createURI(unicode(term))
-        elif type(term) is Literal:
-            return factory.createLiteral(unicode(term),datatype=term.datatype,language=term.language)
-        elif type(term) is BNode:
-            return factory.createBNode(unicode(term))
-        elif type(term) in [list, tuple]:
-            return map(lambda item: toSesame(item,factory), term)
-        return term
-    
-    
-    # STATEMENTS
-    def toStatement((s,p,o),factory,context=None):
-        return factory.createStatement(s,p,o,context)
-        
-    def toTuple(statement):
-        return (statement.getSubject(),statement.getPredicate(),statement.getObject(),statement.getContext())
-except ImportError, e:
-    print 'franz libraries not installed ',e
-    def toRdfLib(term):
-        pass
+                    dtype = URIRef(dtype)
 
-    def toSesame(term, factory):
-        pass
+                return Literal(term.getLabel(), lang=term.getLanguage(),
+                                datatype=dtype)
 
-    def toStatement((s,p,o),factory,context = None):
-        pass
+        except Exception, e:
+            print e
+    elif type(term) is fBNode:
+        return BNode(term.getID())
+    elif type(term) in [list,tuple]:
+        return map(toRdfLib, term)
+    return term
 
-    def toTuple(statement):
-        pass 
-    
+def toSesame(term,factory):
+    if type(term) is URIRef:
+        return factory.createURI(unicode(term))
+    elif type(term) is Literal:
+        return factory.createLiteral(unicode(term),datatype=term.datatype,language=term.language)
+    elif type(term) is BNode:
+        return factory.createBNode(unicode(term))
+    elif type(term) in [list, tuple]:
+        return map(lambda item: toSesame(item,factory), term)
+    return term
+
+
+# STATEMENTS
+def toStatement((s,p,o),factory,context=None):
+    return factory.createStatement(s,p,o,context)
+
+def toTuple(statement):
+    return (statement.getSubject(),statement.getPredicate(),statement.getObject(),statement.getContext())
