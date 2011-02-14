bc3c008267496094d63ea4c7db37a29a61447820
Christoph Burgmer <christoph.burgmer@gmail.com>
2011-02-14 18:29:36 +0000

    Merge plugin test code from rdflib plugin, and fix test method name collision (follow up to r316)
    surf.rdflib will fail a lot of tests; we should probably let our users know through them.
    Some can be fixed by applying experimental patches contributed to rdfextras. Biggest issue is rdflib's (rdfextra's) SPARQL query implementation for named graphs, which will try to retrieve a remote URL instead.

--- plugins/surf.rdflib/surf_rdflib/test/test_rdflib.py
+++ plugins/surf.rdflib/surf_rdflib/test/test_rdflib.py
@@ -3,119 +3,31 @@
 from unittest import TestCase
 
 import surf
+from surf.test.plugin import PluginTestMixin
 
-class TestRdfLib(TestCase):
-    """ Tests for sparql_protocol plugin. """
-    
-    def _get_store_session(self):
+class RdflibTestMixin(object):
+
+    def _get_store_session(self, use_default_context = True):
         """ Return initialized SuRF store and session objects. """
-        
+
         # FIXME: take endpoint from configuration file,
         # maybe we can mock SPARQL endpoint.
-        store = surf.Store(reader = "rdflib", writer = "rdflib")
+        kwargs = {"reader": "rdflib",
+                  "writer" : "rdflib"}
+
+        if False: #use_default_context:
+            kwargs["default_context"] = "http://surf_test_graph/dummy2"
+
+        store = surf.Store(**kwargs)
         session = surf.Session(store)
+
+        # Fresh start!
+        #store.clear("http://surf_test_graph/dummy2")
+        #store.clear(URIRef("http://my_context_1"))
+        #store.clear(URIRef("http://other_context_1"))
+        store.clear()
+
         return store, session
-    
-        
-    def test_is_present(self):
-        """ Test that is_present returns True / False.  """
-        
-        _, session = self._get_store_session()
-        Person = session.get_class(surf.ns.FOAF + "Person")
-        john = session.get_resource("http://john", Person)
 
-        self.assertEquals(john.is_present(), False)
-        john.save()
-        self.assertEquals(john.is_present(), True)
-    
-    def test_get_resource_lazy_attr(self):
-        """ Check that attributes are converted into SuRF objects. """
-        
-        _, session = self._get_store_session()
-        Person = session.get_class(surf.ns.FOAF["Person"])
-        
-        p1 = session.get_resource("http://p1", Person)
-        p2 = session.get_resource("http://p2", Person)
-        p1.foaf_knows = p2
-        p2.foaf_knows = p1
-        p1.save()
-        p2.save()
-
-        another_p1 = session.get_resource("http://p1", Person)
-        another_p1.load()
-        another_p2 = another_p1.foaf_knows.first
-
-        # .foaf_knows.first should be SuRF object not URIRef
-        self.assertTrue(isinstance(another_p2, surf.Resource))
-        self.assertTrue(isinstance(another_p2.foaf_knows.first, surf.Resource))
-        
-    def test_all(self):
-        """ Test that resource.all() works. """
-        
-        _, session = self._get_store_session()
-        Person = session.get_class(surf.ns.FOAF["Person"])
-        p1 = session.get_resource("http://p1", Person)
-
-        list(Person.all())
-        list(p1.all())
-
-    def test_all_limit_order(self):
-        
-        _, session = self._get_store_session()
-
-        FoafPerson = session.get_class(surf.ns.FOAF.Person)
-        # John
-        john = session.get_resource("http://john.com/me", FoafPerson)
-        john.foaf_name = "John"
-        john.save()
-        # Jane
-        jane = session.get_resource("http://jane.com/me", FoafPerson)
-        jane.foaf_name = "Jane"
-        jane.save()
-        
-        # This is currently broken in both rdflib 2.4.2 and 
-        # 3.0.0--ordering in SuRF uses OPTIONAL which doesn't seem to work
-        # in rdflib.
-        # See http://code.google.com/p/rdflib/issues/detail?id=92
-        # and http://code.google.com/p/rdfextras/issues/detail?id=8
-        query = FoafPerson.all().limit(10).order(surf.ns.FOAF.name)
-        names = [p.foaf_name.first for p in query]
-        assert "John" in names
-        assert "Jane" in names
-        
-    def test_save_multiple(self):
-        """ Test that saving multiple resources work.  """
-        
-        # Read from different session.
-        _, session = self._get_store_session()
-        Person = session.get_class(surf.ns.FOAF + "Person")
-        
-        rob = session.get_resource("http://Robert", Person)
-        rob.foaf_name = "Robert"
-        michael = session.get_resource("http://Michael", Person)
-        michael.foaf_name = "Michael"
-        
-        store = session.default_store
-        writer = store.writer
-        
-        writer.save(rob, michael)
-        
-        self.assertTrue(rob.is_present())
-        self.assertTrue(michael.is_present())    
-        
-    def test_remove_inverse(self):
-        
-        _, session = self._get_store_session()
-        Person = session.get_class(surf.ns.FOAF + "Person")
-        
-        jane = session.get_resource("http://Jane", Person)
-        mary = session.get_resource("http://Mary", Person)
-        jane.foaf_knows = mary
-        jane.update()
-        
-        # This should also remove <jane> foaf:knows <mary>.
-        mary.remove(inverse = True)
-
-        jane = session.get_resource("http://Jane", Person)
-        self.assertEquals(len(jane.foaf_knows), 0)
-             
+class StandardPluginTest(TestCase, RdflibTestMixin, PluginTestMixin):
+    pass
--- surf/test/plugin/__init__.py
+++ surf/test/plugin/__init__.py
@@ -693,10 +693,114 @@ class PluginTestMixin(object):
         persons = Person.all()
         self.assertEqual(len(persons), 3)
 
-    def test_remove_inverse(self):
+    def test_remove_inverse3(self):
 
         _, session = self._get_store_session(use_default_context=False)
         self._create_persons(session)
+        Person = session.get_class(surf.ns.FOAF + "Person")
+
+        jane = session.get_resource("http://Jane", Person)
+        mary = session.get_resource("http://Mary", Person)
+        jane.foaf_knows = mary
+        jane.update()
+
+        # This should also remove <jane> foaf:knows <mary>.
+        mary.remove(inverse = True)
+
+        jane = session.get_resource("http://Jane", Person)
+        self.assertEquals(len(jane.foaf_knows), 0)
+
+    # rdflib test cases
+
+    def test_is_present2(self):
+        """ Test that is_present returns True / False.  """
+
+        _, session = self._get_store_session(use_default_context=False)
+        Person = session.get_class(surf.ns.FOAF + "Person")
+        john = session.get_resource("http://john", Person)
+
+        self.assertEquals(john.is_present(), False)
+        john.save()
+        self.assertEquals(john.is_present(), True)
+
+    def test_get_resource_lazy_attr(self):
+        """ Check that attributes are converted into SuRF objects. """
+
+        _, session = self._get_store_session(use_default_context=False)
+        Person = session.get_class(surf.ns.FOAF["Person"])
+
+        p1 = session.get_resource("http://p1", Person)
+        p2 = session.get_resource("http://p2", Person)
+        p1.foaf_knows = p2
+        p2.foaf_knows = p1
+        p1.save()
+        p2.save()
+
+        another_p1 = session.get_resource("http://p1", Person)
+        another_p1.load()
+        another_p2 = another_p1.foaf_knows.first
+
+        # .foaf_knows.first should be SuRF object not URIRef
+        self.assertTrue(isinstance(another_p2, surf.Resource))
+        self.assertTrue(isinstance(another_p2.foaf_knows.first, surf.Resource))
+
+    def test_all2(self):
+        """ Test that resource.all() works. """
+
+        _, session = self._get_store_session(use_default_context=False)
+        Person = session.get_class(surf.ns.FOAF["Person"])
+        p1 = session.get_resource("http://p1", Person)
+
+        list(Person.all())
+        list(p1.all())
+
+    def test_all_limit_order(self):
+
+        _, session = self._get_store_session(use_default_context=False)
+
+        FoafPerson = session.get_class(surf.ns.FOAF.Person)
+        # John
+        john = session.get_resource("http://john.com/me", FoafPerson)
+        john.foaf_name = "John"
+        john.save()
+        # Jane
+        jane = session.get_resource("http://jane.com/me", FoafPerson)
+        jane.foaf_name = "Jane"
+        jane.save()
+
+        # This is currently broken in both rdflib 2.4.2 and
+        # 3.0.0--ordering in SuRF uses OPTIONAL which doesn't seem to work
+        # in rdflib.
+        # See http://code.google.com/p/rdflib/issues/detail?id=92
+        # and http://code.google.com/p/rdfextras/issues/detail?id=8
+        query = FoafPerson.all().limit(10).order(surf.ns.FOAF.name)
+        names = [p.foaf_name.first for p in query]
+        assert "John" in names
+        assert "Jane" in names
+
+    def test_save_multiple2(self):
+        """ Test that saving multiple resources work.  """
+
+        # Read from different session.
+        _, session = self._get_store_session(use_default_context=False)
+        Person = session.get_class(surf.ns.FOAF + "Person")
+
+        rob = session.get_resource("http://Robert", Person)
+        rob.foaf_name = "Robert"
+        michael = session.get_resource("http://Michael", Person)
+        michael.foaf_name = "Michael"
+
+        store = session.default_store
+        writer = store.writer
+
+        writer.save(rob, michael)
+
+        self.assertTrue(rob.is_present())
+        self.assertTrue(michael.is_present())
+
+    def test_remove_inverse4(self):
+
+        _, session = self._get_store_session(use_default_context=False)
         Person = session.get_class(surf.ns.FOAF + "Person")
 
         jane = session.get_resource("http://Jane", Person)
