85865d6836ea5b86bfd36c6e74285439748c2a17
Cosmin Basca <cosmin.basca@gmail.com>
2016-05-04 17:15:08 +0300

    migrate test_query_reader

--- surf/test/plugin/test_query_reader.py
+++ surf/test/plugin/test_query_reader.py
@@ -1,27 +1,26 @@
 # coding=UTF-8
-""" Module for SPARQL generation tests. """
+import pytest
 import logging
-
-from unittest import TestCase
 import warnings
-
-from surf import ns
 from surf.plugin.query_reader import RDFQueryReader
 
-class TestQueryReader(TestCase):
-    """ Tests for query_reader module. """
-    
-    def test_convert_unicode_exception(self):
-        """ Test RDFQueryReader.convert() handles exceptions with unicode. """
 
-        class MyQueryReader(RDFQueryReader):
-            # We want convert() to catch an exception.
-            # Cannot override __convert and throw from there,
-            # but we know __convert calls _to_table... 
-            def _to_table(self, _):
-                warnings.simplefilter("ignore")
-                raise Exception(u"This is unicode: ā")
-            
+def test_convert_unicode_exception():
+    """
+    Test RDFQueryReader.convert() handles exceptions with unicode.
+    """
+
+    class MyQueryReader(RDFQueryReader):
+        # We want convert() to catch an exception.
+        # Cannot override __convert and throw from there,
+        # but we know __convert calls _to_table...
+        def _to_table(self, _):
+            warnings.simplefilter("ignore")
+            raise Exception(u"This is unicode: ā")
+
+    try:
         logging.disable(logging.ERROR)
         MyQueryReader().convert(None)
         logging.disable(logging.NOTSET)
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
