318cce0c55c8f544ae4429e423cb06d06967750a
Cosmin Basca <cosmin.basca@gmail.com>
2016-05-04 16:59:00 +0300

    migrate test_session

--- surf/test/test_session.py
+++ surf/test/test_session.py
@@ -1,21 +1,21 @@
  # coding=UTF-8
-""" Module for surf.session.Session tests. """
-
-from unittest import TestCase
+import pytest
 
 from surf import Session, Store
 
-class TestSession(TestCase):
-    """ Tests for Session class. """
-    
-    def test_close_multiples_stores(self):
-        """ Test that closing a session with multiple stores work. """
 
+def test_close_multiples_stores():
+    """
+    Test that closing a session with multiple stores work.
+    """
+    try:
         store1 = Store()
         session = Session(store1)
 
         store2 = Store()
         session["store2"] = store2
-        
+
         # Should not fail.
         session.close()
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
