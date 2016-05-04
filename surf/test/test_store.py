ac6ac85ec554bd0362acb52f90bb62f739896de6
Cosmin Basca <cosmin.basca@gmail.com>
2016-05-04 17:01:46 +0300

    migrate test_store

--- surf/test/test_store.py
+++ surf/test/test_store.py
@@ -1,63 +1,76 @@
 # coding=UTF-8
-""" Module for surf.store.Store tests. """
+import pytest
 
 import logging
 from unittest import TestCase
-import warnings
 import surf
 from surf import Session, Store
 from surf.plugin.reader import RDFReader
 from surf.plugin.writer import RDFWriter
 
-class TestStore(TestCase):
-    """ Tests for Store class. """
-    
-    def test_multiples(self):
-        """ Test synchronization between empty attribute and rdf_direct. """
 
-        store = Store(log_level=logging.NOTSET)
-        session = Session(store)
-        
-        Person = session.get_class(surf.ns.FOAF.Person)
-        
-        rob = session.get_resource("http://Robert", Person)
-        rob.foaf_name = "Robert"
-        michael = session.get_resource("http://Michael", Person)
-        michael.foaf_name = "Michael"
-        
-        # Should not fail.
+def test_multiples():
+    """
+    Test synchronization between empty attribute and rdf_direct.
+    """
+
+    store = Store(log_level=logging.NOTSET)
+    session = Session(store)
+
+    Person = session.get_class(surf.ns.FOAF.Person)
+
+    rob = session.get_resource("http://Robert", Person)
+    rob.foaf_name = "Robert"
+    michael = session.get_resource("http://Michael", Person)
+    michael.foaf_name = "Michael"
+
+    try:
         store.save(rob, michael)
         store.update(rob, michael)
         store.remove(rob, michael)
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
 
-    def test_close_unicode_exception(self):
-        """ Test that closing a store handles exceptions. """
 
-        class MockReader(RDFReader):
-            def close(self):
-                raise Exception(u"Some unicode: ā")
+def test_close_unicode_exception():
+    """
+    Test that closing a store handles exceptions.
+    """
 
-        class MockWriter(RDFWriter):
-            def close(self):
-                raise Exception(u"Some unicode: ā")
+    class MockReader(RDFReader):
+        def close(self):
+            raise Exception(u"Some unicode: ā")
 
+    class MockWriter(RDFWriter):
+        def close(self):
+            raise Exception(u"Some unicode: ā")
+
+    try:
         reader = MockReader()
         store = Store(reader, MockWriter(reader), log_level=logging.NOTSET)
         logging.disable(logging.ERROR)
         store.close()
         logging.disable(logging.NOTSET)
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
 
-    def test_successful_close(self):
-        """ Test that store handles successful reader and writer closes. """
-        
-        class MockReader(RDFReader):
-            def close(self):
-                pass
 
-        class MockWriter(RDFWriter):
-            def close(self):
-                pass
-        
+def test_successful_close():
+    """
+    Test that store handles successful reader and writer closes.
+    """
+
+    class MockReader(RDFReader):
+        def close(self):
+            pass
+
+    class MockWriter(RDFWriter):
+        def close(self):
+            pass
+
+    try:
         reader = MockReader()
         store = Store(reader, MockWriter(reader), log_level=logging.NOTSET)
         store.close()
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
