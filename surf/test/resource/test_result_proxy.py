77e148bdeea01786aea304c5f4169eafe916e1bb
Cosmin Basca <cosmin.basca@gmail.com>
2016-05-04 18:20:23 +0300

    migrate test_result_proxy

--- surf/test/resource/test_result_proxy.py
+++ surf/test/resource/test_result_proxy.py
@@ -1,11 +1,9 @@
-""" Module for ResultProxy tests. """
-
-import unittest
+import pytest
 import surf
 from surf.resource.result_proxy import ResultProxy
 
-class MockStore(object):
 
+class MockStore(object):
     def __init__(self):
         self.__expect_args = {}
         self.__data = []
@@ -22,72 +20,114 @@ class MockStore(object):
 
         return self.__data
 
+
 class MockResource(object):
     subject = "mock_subject"
+
 
 def mock_instance_factory(params, instance_data):
     return "instance"
     
 
-class TestResultProxy(unittest.TestCase):
-
-    def setUp(self):
-        self.store = MockStore()
-        params = { "store" : self.store, "instance_factory" : mock_instance_factory}
-        self.proxy = ResultProxy(params)
-
-    def test_all_empty_list(self):
-        """ Test ResultProxy with no filters, no returned data. """
-        
-        results = list(self.proxy)
-        assert len(results) == 0
-
-    def test_limit_offset(self):
-        """ Test limit, offset. """
-        
-        self.store.expect_args({"limit" : 10, "offset" : 5})
-        list(self.proxy.limit(10).offset(5))
-
-    def test_full(self):
-        """ Test full(). """
-        
-        self.store.expect_args({'full' : True, 'direct_only' : True})
-        list(self.proxy.full(direct_only = True))
+@pytest.fixture
+def store_proxy():
+    store = MockStore()
+    params = {"store": store, "instance_factory": mock_instance_factory}
+    proxy = ResultProxy(params)
+    return store, proxy
 
 
-    def test_order_desc(self):
-        """ Test order, desc. """
-        
-        self.store.expect_args({"order" : "some_attr", "desc" : True})
-        list(self.proxy.order("some_attr").desc())
+def test_all_empty_list(store_proxy):
+    """
+    Test ResultProxy with no filters, no returned data.
+    """
+    _, proxy = store_proxy
+    results = list(proxy)
+    assert len(results) == 0
 
-    def test_get_by(self):
-        """ Test get_by. """
-        
+
+def test_limit_offset(store_proxy):
+    """
+    Test limit, offset.
+    """
+    store, proxy = store_proxy
+    try:
+        store.expect_args({"limit": 10, "offset": 5})
+        list(proxy.limit(10).offset(5))
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
+
+
+def test_full(store_proxy):
+    """
+    Test full().
+    """
+    store, proxy = store_proxy
+    try:
+        store.expect_args({'full': True, 'direct_only': True})
+        list(proxy.full(direct_only=True))
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
+
+
+def test_order_desc(store_proxy):
+    """
+    Test order, desc.
+    """
+    store, proxy = store_proxy
+    try:
+        store.expect_args({"order": "some_attr", "desc": True})
+        list(proxy.order("some_attr").desc())
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
+
+
+def test_get_by(store_proxy):
+    """
+    Test get_by.
+    """
+    store, proxy = store_proxy
+    try:
         expected = [(surf.ns.FOAF["name"], "Jane", True)]
-        self.store.expect_args({"get_by" : expected})
-        list(self.proxy.get_by(foaf_name = "Jane"))
+        store.expect_args({"get_by": expected})
+        list(proxy.get_by(foaf_name="Jane"))
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
 
-    def test_context(self):
-        """ Test context. """
-        
-        self.store.expect_args({"context" : "my_context"})
-        list(self.proxy.context("my_context"))
 
-    def test_filter(self):
-        """ Test filter. """
-        
-        self.store.expect_args({"filter" : [(surf.ns.FOAF["name"], "f", True)]})
-        list(self.proxy.filter(foaf_name = "f"))
+def test_context(store_proxy):
+    """
+    Test context.
+    """
+    store, proxy = store_proxy
+    try:
+        store.expect_args({"context": "my_context"})
+        list(proxy.context("my_context"))
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
 
-    def test_get_by_resource(self):
-        """ Test that get_by accepts Resources as values. """
-        
+
+def test_filter(store_proxy):
+    """
+    Test filter.
+    """
+    store, proxy = store_proxy
+    try:
+        store.expect_args({"filter": [(surf.ns.FOAF["name"], "f", True)]})
+        list(proxy.filter(foaf_name="f"))
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
+
+
+def test_get_by_resource(store_proxy):
+    """
+    Test that get_by accepts Resources as values.
+    """
+    store, proxy = store_proxy
+    try:
         resource = MockResource()
         expected = [(surf.ns.FOAF["knows"], resource.subject, True)]
-        self.store.expect_args({"get_by" : expected})
-        list(self.proxy.get_by(foaf_knows = resource))
-
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+        store.expect_args({"get_by" : expected})
+        list(proxy.get_by(foaf_knows = resource))
+    except Exception, e:
+        pytest.fail(e.message, pytrace=True)
