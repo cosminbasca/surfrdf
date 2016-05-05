ae4f43eca67c92794fcbad910c1a0c899703dffa
Cosmin Basca <cosmin.basca@gmail.com>
2016-05-05 11:52:16 +0300

    pep8 and docs update for module: rest

--- surf/rest.py
+++ surf/rest.py
@@ -33,82 +33,133 @@
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # -*- coding: utf-8 -*-
+import inspect
+from surf.rdf import Namespace
+from surf.resource import Resource
+
 __author__ = 'Cosmin Basca'
 
-from surf.rdf import Namespace
 
 # TODO lazy loading ... not implemented when setting the resource from the json_dict ...
 class Rest(object):
-    ''' The :class:`Rest` class handles the generation of REST like methods to
-    perform CRUD operations on a :class:`surf.resource.Resource` class
+    """
+    The :class:`Rest` class handles the generation of REST like methods to
+    perform CRUD operations on a :class:`surf.resource.Resource` instance
 
     note: The REST api exposed is designed in accordance with the REST controller
     used in `pylons` applications, it adheres to the REST specification and offers
-    extra features'''
-    def __init__(self, resources_namespace, concept_class):
-        '''the `resource` is the :class:`surf.resource.Resource` class for which
-        the REST interface is exposed,
-        the `resources_namespace` represents the URI that instances will be using as
-        subjects'''
+    extra features
 
-        self.__concept_class = concept_class
+    :param resources_namespace: the `uri` used by instances
+    :type resources_namespace: :class:`rdflib.namespace.Namespace` or str
+    :param concept_class: the class for which the REST interface is exposed
+    :type concept_class: :class:`surf.resource.Resource`
+    """
+
+    def __init__(self, resources_namespace, concept_class):
+        assert issubclass(concept_class, Resource) and inspect.isclass(concept_class)
+        self._Resource = concept_class
 
         # Make sure resources_namespace is an instance of Namespace
         if not isinstance(resources_namespace, Namespace):
             resources_namespace = Namespace(resources_namespace)
-        
-        self.__namespace = resources_namespace
 
-    # the REST methods
-    def index(self, offset = None, limit = None):
-        '''**REST** : GET /: All items in the collection,
-        returns all `instances` for the current `Resource`'''
-        return self.__concept_class.all(offset = offset, limit = limit)
+        self._namespace = resources_namespace
+
+    def index(self):
+        """
+        **REST** : GET /: All items in the collection,
+
+        :return: all instances for the current :class:`surf.resources.Resource`
+        :rtype: list or :class:`surf.resources.ResultProxy`
+        """
+        return self._Resource.all()
 
     def create(self, json_params):
-        '''**REST** : POST /: Create a new item,
-        creates a new instance of the current `Resource` type'''
-        instance = self.__concept_class()
+        """
+        **REST** : POST /: Create a new item,
+        creates a new :class:`surf.resource.Resource` instance
+
+        :param dict json_params: extra *JSON* parameters to set as instance attributes
+        """
+        instance = self._Resource()
         for attr_name in json_params:
             setattr(instance, attr_name, json_params[attr_name])
         instance.save()
 
     def new(self, json_params):
-        '''**REST** : GET /new: Form to create a new item.
-        creates a new instance of the current `Resource` type'''
+        """
+        **REST** : GET /new: Form to create a new item.
+        alias for :meth:`surf.rest.Rest.create` method
+
+        :param dict json_params: extra *JSON* parameters to set as instance attributes
+        """
         self.create(json_params)
 
     def update(self, id, json_params):
-        '''**REST** : PUT /id: Update an existing item.,
-        update an instnaces attributes with the supplied parameters'''
-        instance = self.__concept_class(self.__namespace[id])
+        """
+        **REST** : PUT /id: Update an existing item.,
+        update an instances attributes with the supplied parameters
+
+        :param str id: the resources id
+        :param dict json_params: extra *JSON* parameters to set as instance attributes
+        """
+        instance = self._Resource(self._namespace[id])
         for attr_name in json_params:
             setattr(instance, attr_name, json_params[attr_name])
         instance.update()
 
     def edit(self, id, json_params):
-        '''**REST** : GET /id;edit:
-        updates an instances attributes with the supplied parameters'''
+        """
+        **REST** : GET /id;edit:
+        alias for :meth:`surf.rest.Rest.update` method
+
+        :param str id: the resources id
+        :param dict json_params: extra *JSON* parameters to set as instance attributes
+        """
         self.update(id, json_params)
 
     def delete(self, id):
-        '''**REST** : DELETE /id: Delete an existing item.
-        removes the denoted instance from the underlying `store`'''
-        instance = self.__concept_class(self.__namespace[id])
+        """
+        **REST** : DELETE /id: Delete an existing item.
+        removes an instance from the underlying :class:`surf.store.Store` store
+
+        :param str id: the resources id
+        """
+        instance = self._Resource(self._namespace[id])
         instance.remove()
 
     def show(self, id):
-        '''**REST** : GET /id: Show a specific item.
-        show / retrieve the specified resource'''
-        instance = self.__concept_class(self.__namespace[id])
+        """
+        **REST** : GET /id: Show a specific item.
+        show / retrieve the specified resource
+
+        :param str id: the resources id
+        :return: the resource
+        :rtype: :class:`surf.resources.Resource`
+        """
+        instance = self._Resource(self._namespace[id])
         instance.load()
         return instance
 
     @classmethod
-    def resource(cls, session, resources_namespace, concept, id):
+    def resource(cls, session, resources_namespace, concept_class, id):
+        """
+        convenience method to get a resource given the arguments
+
+        :param session: the *SuRF* session
+        :type session: :class:`surf.session.Session`
+        :param resources_namespace: the `uri` used by instances
+        :type resources_namespace: :class:`rdflib.namespace.Namespace` or str
+        :param concept_class: the class for which the REST interface is exposed
+        :type concept_class: :class:`surf.resource.Resource`
+        :param str id: the resources id
+        :return: the resource
+        :rtype: :class:`surf.resources.Resource`
+        """
         # Make sure resources_namespace is an instance of Namespace
         if not isinstance(resources_namespace, Namespace):
             resources_namespace = Namespace(resources_namespace)
 
-        Concept = session.get_class(concept)
-        return Concept(resources_namespace[id])
+        _Resource = session.get_class(concept_class)
+        return _Resource(resources_namespace[id])
